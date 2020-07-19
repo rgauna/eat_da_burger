49271b9d3db1a9b4882ee1a85caf459be230e5af
rudy gauna <rudy.gauna@gmail.com>
2020-07-18 23:21:07 -0500

    updated server.js file with middleware definitions

--- server.js
+++ server.js
@@ -5,3 +5,26 @@ var path = require('path')
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+
+
+// servers the public static pages in the appliction directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(methodOverride('_method'))
+var exphbs = require('express-handlebars');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+var routes = require('./controllers/burgers_controllers.js');
+app.use('/', routes);
+
+
+app.listen(PORT);
