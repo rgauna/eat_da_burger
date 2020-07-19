75f239a9f729ff0de8150df1033876420c8260c1
rudy gauna <rudy.gauna@gmail.com>
2020-07-18 22:08:05 -0500

    created & populated seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers(burger_name) values ('Bye, son! Burger');
+insert into burgers(burger_name) values ('Good Burger');
+insert into burgers(burger_name) values ('The Luther');
+insert into burgers(burger_name) values ('FAT BURGER');
