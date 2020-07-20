ffc594f9a519000617d6592b7f8899b9ca1190b4
rudy gauna <rudy.gauna@gmail.com>
2020-07-19 23:28:27 -0500

    fixing procfile due to getting H13 error in heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -19,7 +21,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-var router = require('./controllers/burgers_controller.js');
+var router = require('./controllers/burgers_controller');
 app.use('/', router);
 
 
